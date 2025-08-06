e443b40aa6d46f90384ed1a35022ab8592bf8a35
AkashVinoo <akashvinoo@gmail.com>
2025-08-06 23:57:10 +0530

    Clean repo: keep only Data Analyst Agent project files

--- api.py
+++
@@ -1,5 +0,0 @@
-# Import the Wikipedia API from main.py
-from main import app
-
-# The app is already configured as a FastAPI application
-# This file just makes it available for run_server.py to import 
